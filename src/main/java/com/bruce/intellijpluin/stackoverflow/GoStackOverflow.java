13e7e80d903aea0c707dc583e54381b7b490c7cb
Jack Chu <chuchuanbao@gmail.com>
2019-01-25 17:27:46 +0800

    feat: 搜索引擎支持Baidu

--- src/main/java/com/bruce/intellijpluin/stackoverflow/GoStackOverflow.java
+++ src/main/java/com/bruce/intellijpluin/stackoverflow/GoStackOverflow.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.ui.Messages;
 import org.apache.commons.lang.StringUtils;
 
 import javax.swing.*;
@@ -18,19 +19,25 @@ import javax.swing.*;
 public class GoStackOverflow extends AnAction {
 
     public GoStackOverflow(){
-        super(null,null, new ImageIcon(GoStackOverflow.class.getClassLoader().getResource("icon/stackoverflow.png")));
+        super(null,null, new ImageIcon(GoStackOverflow.class.getClassLoader().getResource("icon/stackoverflow.ico")));
     }
     @Override
     public void actionPerformed(AnActionEvent e) {
-        String googleSite = "https://www.google.com/";
+        String desc = this.getTemplatePresentation().getDescription();
+        String baseUrl;
+        if ("google".equalsIgnoreCase(desc)){
+            baseUrl = "https://www.google.com/?#newwindow=1&q=site:stackoverflow.com ";
+        }else{
+            baseUrl = "https://www.baidu.com/s?si=stackoverflow.com&ct=2097152&wd=";
+        }
         CaretModel caretModel = e.getData(LangDataKeys.EDITOR).getCaretModel();
         Caret currentCaret =
                 caretModel.getCurrentCaret();
         String selectedText = currentCaret.getSelectedText();
         if (StringUtils.isNotBlank(selectedText)) {
-            BrowserUtil.browse(googleSite + "?#newwindow=1&q=site:stackoverflow.com " + selectedText + "&*");
+            BrowserUtil.browse(baseUrl + selectedText + "&*");
         } else {
-            BrowserUtil.browse(googleSite + "?#newwindow=1&q=site:stackoverflow.com " + "&*");
+            BrowserUtil.browse(baseUrl + "&*");
         }
     }
 }
